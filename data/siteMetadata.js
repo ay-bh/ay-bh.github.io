38427544c93969921a2c9b27bd57e1c96e858602
Ayush Bhardwaj <96904351+ay-bh@users.noreply.github.com>
2024-11-03 13:29:25 +0530

    Update siteMetadata.js

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,9 +1,9 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'Ayush Bhardwaj',
+  author: 'Ayush Bhardwaj',
+  headerTitle: 'ay-bh',
+  description: 'My debug console for life and learning',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
