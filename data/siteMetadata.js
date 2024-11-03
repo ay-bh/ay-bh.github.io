bb717f4720732de46de124f37434bbdcb254a12d
Ayush Bhardwaj <96904351+ay-bh@users.noreply.github.com>
2024-11-03 13:32:17 +0530

    Update siteMetadata.js

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,8 +1,8 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Ayush Bhardwaj',
+  title: 'ay-bh',
   author: 'Ayush Bhardwaj',
-  headerTitle: 'ay-bh',
+  headerTitle: 'Ayush Bhardwaj',
   description: 'My debug console for life and learning',
   language: 'en-us',
   theme: 'system', // system, dark or light
